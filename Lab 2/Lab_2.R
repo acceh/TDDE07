02d183a564ba5e14d3770df110c7c7f565165c01
Axel Holmberg <acceh2@gmail.com>
2020-04-27 16:58:57 +0200

    Done until 2b)

--- Lab 2/Lab_2.R
+++ Lab 2/Lab_2.R
@@ -50,3 +50,96 @@ for (i in 2:nrow(linear_matrix)) {
 	lines(times, linear_matrix[i,])
 	
 }
+
+
+################################### Ass 2 #####################################################
+library("mvtnorm") # This command reads the mvtnorm package into R's memory. NOW we can use dmvnorm function.
+
+data<-read.table("WomenWork.dat",header=TRUE)  # Spam data from Hastie et al.
+
+y <- as.vector(data$Work)
+X <- as.matrix(data[,2:9])
+
+
+tau <- 10
+mu <- rep(0, 8)
+sigma <- tau^2 * diag(8)
+
+initVal <- rmvnorm(1, mu, sigma)
+
+
+
+LogPostLogistic <- function(betaVect,y,X,mu,Sigma){
+	nPara <- length(betaVect);
+	linPred <- X%*%betaVect;
+	print(betaVect)
+	# evaluating the log-likelihood                                    
+	logLik <- sum( linPred*y -log(1 + exp(linPred)));
+	if (abs(logLik) == Inf) logLik = -20000; # Likelihood is not finite, stear the optimizer away from here!
+	
+	# evaluating the prior
+	logPrior <- dmvnorm(betaVect, matrix(0,nPara,1), Sigma, log=TRUE);
+	print(logLik + logPrior)
+	print(betaVect)
+	# add the log prior and log-likelihood together to get log posterior
+	return(logLik + logPrior)
+}
+
+
+OptimResults<-optim(initVal,LogPostLogistic,gr=NULL,y,X,mu,sigma,method=c("BFGS"),control=list(fnscale=-1),hessian=TRUE)
+
+inverse_hessian <- solve(OptimResults$hessian)
+
+postModeNSmallChild <- OptimResults$par[7]
+stdPostNSmallChild <- sqrt(diag(-inverse_hessian)[7])
+
+
+
+betaGrid <-
+	seq(
+		postModeNSmallChild - 4 * stdPostNSmallChild,
+		postModeNSmallChild + 4 * stdPostNSmallChild,
+		length = 1000
+	)
+ciBetaNsmallChild <-
+	quantile(rnorm(10000, mean = postModeNSmallChild, sd = stdPostNSmallChild),
+						 c(0.025, 0.975))
+ggplot() + geom_line(aes(
+	x = betaGrid,
+	y = dnorm(x = betaGrid, mean = postModeNSmallChild, sd = stdPostNSmallChild)
+)) + geom_vline(xintercept = ciBetaNsmallChild[1]) + geom_vline(xintercept = ciBetaNsmallChild[2])
+
+
+glmModel <- glm(Work ~ 0 + ., data = data, family = binomial)
+
+glmSmallChildVar <- glmModel[["coefficients"]][["NSmallChild"]]
+
+#b)
+
+beta_posterior <- OptimResults$par
+
+features <- c(1, 10.0, 8,(8/10)^2 , 10, 40, 1, 1)
+
+prob_working_women <- (exp(t(features)%*%t(beta_posterior))) / (1+exp(t(features)%*%t(beta_posterior)))
+
+func_working_women_prob <- function(feature, betaposterior) {
+	tmp <- (exp(t(feature) %*% betaposterior)) / (1 + exp(t(feature) %*% betaposterior))
+	return(tmp)
+}
+
+stdbeta <- diag(-inverse_hessian)
+
+sim_beta <- rmvnorm(10000, mean=beta_posterior, sigma=-solve(OptimResults$hessian))
+
+tmp <- c()
+for(i in 1:nrow(sim_beta)) {
+
+	tmp <- rbind(tmp,(func_working_women_prob(features, sim_beta[i,])))
+}
+
+barplot(tmp)
+
+tmp_list <- ifelse(tmp>0.5, 1, 0)
+
+
+
